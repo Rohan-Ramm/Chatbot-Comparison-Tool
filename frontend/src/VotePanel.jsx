c244884d82e22c1ebdb7cabfba4fc18f56672c64
Rohan <ramma006@umn.edu>
2026-06-25 21:26:45 -0500

    fix spacing

--- frontend/src/VotePanel.jsx
+++ frontend/src/VotePanel.jsx
@@ -4,12 +4,12 @@ function VotePanel({ scoresChangeable, model1, model2, onModel1Vote, onModel2Vot
   return (
     <p className="after-response">
       <b>Which gave better advice?</b>
-      <p className="bullet-point">
+      <div className="bullet-point">
         {model1} <button className="tiny-button" id="model1" onClick={onModel1Vote}><i className="fa fa-thumbs-up"></i></button>
-      </p>
-      <p className="bullet-point">
+      </div>
+      <div className="bullet-point">
         {model2} <button className="tiny-button" id="model2" onClick={onModel2Vote}><i className="fa fa-thumbs-up"></i></button>
-      </p>
+      </div>
     </p>
   )
 }
